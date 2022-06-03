ce3ab4ca572b71e41504690b1226f9ffb12c1fc4
Victor_Reyes <jvreyesa@hotmail.com>
2022-06-02 20:22:34 -0500

    clase 12 tarea

--- js/homework.js
+++ js/homework.js
@@ -1,4 +1,4 @@
-
+/*
 let salarios = {
    'Juan': 30000,
    'Alberto': 50000,
@@ -64,8 +64,165 @@ koder.edad = 30
 
 console.log( koder )
 
+
 console.log( koder.languajes)
 
 let objClon = koder
 
 console.log("objeto clonado", objClon )
+
+
+*/
+
+/**
+ * Dado un array de objetos koders
+ * Imprimir en consola, todos los nombres de cada koder
+ * 
+ * 'jorge luis Camarillo tiene 30 años y es de la generacion 6'
+ * 'Erik Gutierrez tiene 30 años y es de la generacion 16'
+ * ....
+ * 
+ */
+
+ let koders =  [
+    {
+        name: 'jorge luis',
+        lastName: 'Camarillo',
+        age: 30,
+        generation: 6,
+        modulos: ['js','node js', 'cloud'],
+    },
+    {
+        name: 'Erik',
+        lastName: 'Gutierrez',
+        age: 20,
+        generation: 15,
+        modulos: ['js'],
+    },
+    {
+        name: 'Sara',
+        lastName: 'Reveles',
+        age: 24,
+        generation: 12,
+        modulos: ['js'],
+    }
+]
+
+const arrManager = ( arrKoders ) => {
+
+    for(i = 0; i< arrKoders.length ; i++){
+        
+        console.log(`
+            ${arrKoders[i].name}  ${arrKoders[i].lastname} 
+            tiene ${arrKoders[i].age} años y es 
+            de la generacion ${arrKoders[i].generation}.
+        `);
+    }
+
+    //solucion Jorge
+    arrKoders.forEach((ObjCompleto, index, arrayObj)=>{
+
+        console.log(`
+            ${ObjCompleto.name} ${ObjCompleto.lastname} 
+            tiene ${ObjCompleto.age} años y es 
+            de la generacion ${ObjCompleto.generation}.`);
+
+    })
+}
+
+arrManager( koders )
+
+/**
+ * Del mismo array de koders
+ * Obtener la suma de todas las edades
+ * 
+ */
+
+ const arrManagerSuma = ( arrKoders ) => {
+    
+    let sumaEdad = 0
+
+    for(i = 0; i< arrKoders.length ; i++){
+        
+        sumaEdad += parseInt(arrKoders[i].age)
+        
+    }
+
+    console.log(`la suma de las edades es ${sumaEdad}.`);
+
+     //solucion Jorge
+     sumaEdad = 0
+
+     arrKoders.forEach((ObjCompleto, index, arrayObj)=>{
+
+        sumaEdad += parseInt(ObjCompleto.age)
+        
+
+    })
+
+    console.log(`la suma de las edades es ${sumaEdad}.`);
+}
+
+arrManagerSuma( koders )
+
+
+/**
+ * Del objeto library
+ * 1. Obtener el numero de libros que se estan leyendo
+ * 2. Obtener una lista de todos los autores
+ * 3. Obtener una lista de todos los Libros
+ * 
+ */
+ let library = [ 
+    {
+        author: 'Bill Gates',
+        title: 'The Road Ahead',
+        readingStatus: true
+    },
+    {
+        author: 'Steve Jobs',
+        title: 'Walter Isaacson',
+        readingStatus: true
+    },
+    {
+        author: 'Suzanne Collins',
+        title:  'Mockingjay: The Final Book of The Hunger Games', 
+        readingStatus: false
+    }];
+
+
+    const arrManagerLibrary = ( arrLibrary ) => {
+
+        let sumaLibros =  arrLibrary.length  
+
+        console.log(`los libros leidos son ${sumaLibros}.`);
+
+        arrLibrary.forEach((ObjCompleto, index, arrayObj)=>{
+
+            console.log(`Autor:  ${ObjCompleto.author} - Titulo:  ${ObjCompleto.title} .`);
+          
+        })
+
+
+    }
+    
+    arrManagerLibrary( library )
+
+    let totalReading = library.reduce(( total, libro) => {
+        return libro.readingStatus ? total += 1: total
+    }, 0)
+
+    console.log( totalReading )
+    
+
+    let authors = library.map( (author)=>{
+        return author.author
+    })
+
+    console.log( authors )
+
+    let titles = library.map( (title)=>{
+        return title.title
+    })
+
+    console.log( titles )
