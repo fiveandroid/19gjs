5f9cc474350b115a7a19fffebdae9120adf734c9
Victor_Reyes <jvreyesa@hotmail.com>
2022-06-08 19:25:31 -0500

    clase 16

---
+++ js/bom.js
@@ -0,0 +1,45 @@
+/*
+    DOM Document Object Model
+    Manipilar el documento html
+
+        document.querySelector('item')
+        element.classList.add('clasenueva')
+        element.innerHTML = 'nuevohtml'
+        element.addAttribute('propiedad', 'valor')
+        element.addAttribute('propiedad')
+        document.getElementsByTagName('h2')[1]
+        document.createElement('div')
+        document.getElementById(iddelinput).value
+
+        callback es una funcion pasada como parametro
+
+        element.addEventListener('click', callback )
+        element.addEventListener('click', () => { ... } )  -> Funcion anonima
+
+        100vw = 1680px
+        10vw = 168 px
+        1vw = 16.8
+
+    BOM Browser Object Model
+    Comunicarse con el navegador
+
+    window
+    window.innerWidt;
+    window.innerHeight;
+    window.screen; // propiedades de la pantalla
+
+    window.navigator // Propiedades dek navegador
+    window.history.back()  // hacia la pagina anterior
+    windows.history.forward() // hacia la siguiente pagina abierta
+
+    window.location 
+
+    letscripts certificados
+
+
+    Ajax
+    Que es
+    Diferentes formas de implementarlo
+    
+
+*/
